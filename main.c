0b3e0bbb083eb21c1058be4be9482ad10157edfd
Sanjeev Tripathi <sanjeev309@users.noreply.github.com>
2016-05-08 18:38:03 +0530

    Added required comments

--- main.c
+++ main.c
@@ -5,21 +5,21 @@
 
 int main(void)
 {   int n=0;
-   DDRD=0xff;
+   DDRD=0xff;          // Set port D as output
    
-     while (1) 
+     while (1)                // Infinite loop
     { 
 		
 		 while(n!=7)
-			{ PORTD=(1<<n)|(1<<(n+1));
-			_delay_ms(6);
+			{ PORTD=(1<<n)|(1<<(n+1));           //Stream two led's across at a time :Right 
+			_delay_ms(6);                         
 			n++;	
 			  }
 	
 	   	  while(n!=0)
-			 { PORTD=(1<<n)|(1<<(n-1));
+			 { PORTD=(1<<n)|(1<<(n-1));         //Left
 			 _delay_ms(6);
-	        n--;
+	                  n--;
  			  }
 	}
 }
